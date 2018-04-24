006240d09d5ca73395beca8b11683f2cfd8254c5
ポール ウェッブ <netopwibby@thenetwork.email>
2018-04-24 14:56:17 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/* jshint undef: true, unused: true, esversion: 6, node: true */
+
+"use strict";
+
+
+
+/*
+  Weather
+  Search
+  Calendar (time/date/events)
+  Math (super easy)
+
+  —
+
+  NetNavi is constantly learning intent and learns new phrases and idioms
+  from you. Maybe Navi can ask what something means?
+
+  Example:
+    Me: "That shit is boof"
+    Navi: "What does `boof` mean?"
+    Me: "Boof means `wack`".
+*/
